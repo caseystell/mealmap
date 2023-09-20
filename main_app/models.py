64cdd422b0a65c557584f4c95b42a33dd4d776c9
caseystell <cleigh14@hotmail.com>
2023-09-20 14:26:28 -0600

    models, views, urls, and templates updates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+MEALS = (
+    ('Breakfast', 'Breakfast'),
+    ('Lunch', 'Lunch'),
+    ('Dinner', 'Dinner'),
+    ('Snack', 'Snack'),
+)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=150)
+    url = models.URLField(max_length=250)
+
+    def __str__(self):
+        return f'{self.title} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'recipe_id': self.id})
+
+
+class Calendar(models.Model):
+    date = models.DateField('Date')
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.date} ({self.id})'
+
+
+class Meal(models.Model):
+    date = models.DateField('Date')
+    meal = models.CharField(
+        max_length=9,
+        choices=MEALS,
+        default=[0][0]
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import os
+import uuid
+import boto3
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Recipe, Calendar
+from .forms import MealForm
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+class RecipeCreate(CreateView):
+  model = Recipe
+  fields = ['title', 'url']
+
+class RecipeUpdate(UpdateView):
+  model = Recipe
+  fields = ['title', 'url']
+
+class RecipeDelete(DeleteView):
+  model = Recipe
+  success_url = '/recipes'
+
+def add_meal(request, recipe_id):
+  form = MealForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    new_meal = form.save(commit=False)
+    new_meal.recipe_id = recipe_id
+    new_meal.save()
+  return redirect('detail', recipe_id=recipe_id)
