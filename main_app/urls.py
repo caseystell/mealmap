0002a5900bf53437f2d48884f021c9f23940c9e9
caseystell <cleigh14@hotmail.com>
2023-09-22 13:06:42 -0600

    add detail template and update index template

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('recipes/', views.recipes_index, name='index'),
+  path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create'),
+  path('recipes/<int:pk>/update/', views.RecipeUpdate.as_view(), name='recipes_update'),
+  path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipes_delete'),
+  path('recipes/<int:recipe_id>/add_to_calendar/<int:date_id>/', views.add_to_calendar, name='add_to_calendar'),
+  path('recipes/<int:recipe_id>/remove_from_calendar/<int:date_id>/', views.remove_from_calendar, name='remove_from_calendar'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -14,6 +14,12 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+def recipes_index(request):
+    # recipes = Recipe.objects.filter(user=request.user)
+    return render(request, 'recipes/index.html', 
+    #     {'recipes': recipes}
+    )
+
 class RecipeCreate(CreateView):
   model = Recipe
   fields = ['title', 'url']
@@ -33,4 +39,13 @@ def add_meal(request, recipe_id):
     new_meal = form.save(commit=False)
     new_meal.recipe_id = recipe_id
     new_meal.save()
-  return redirect('detail', recipe_id=recipe_id)
+  return redirect('index', recipe_id=recipe_id)
+
+def add_to_calendar(request, recipe_id, date_id):
+   Recipe.objects.get(id=recipe_id).calendar.add(date_id)
+   return redirect('index', recipe_id=recipe_id)
+
+
+def remove_from_calendar(request, recipe_id, date_id):
+   Recipe.objects.get(id=recipe_id).calendar.remove(date_id)
+   return redirect('index', recipe_id=recipe_id)
